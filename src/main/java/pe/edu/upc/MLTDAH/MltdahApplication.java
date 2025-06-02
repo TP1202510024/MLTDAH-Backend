205943b9ca4f8c5e5e6674c3b2c773f9607442b4
Djalma Dioses Molina <dioses1302@gmail.com>
2025-06-02 09:58:28 -0500

    feat: init project

---
+++ src/main/java/pe/edu/upc/MLTDAH/MltdahApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.MLTDAH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MltdahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MltdahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/MLTDAH/MltdahApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.MLTDAH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MltdahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
