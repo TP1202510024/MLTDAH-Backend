c6294c87d9a7a820ec19fd2fe8bab69b8374c340
Djalma Dioses Molina <dioses1302@gmail.com>
2025-06-02 20:53:32 -0500

    feat: add institution command and query service

---
+++ src/main/java/pe/edu/upc/MLTDAH/iam/domain/services/InstitutionQueryService.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.MLTDAH.iam.domain.services;
+
+import pe.edu.upc.MLTDAH.iam.domain.model.aggregates.Institution;
+import pe.edu.upc.MLTDAH.iam.domain.model.queries.GetAllInstitutionsQuery;
+import pe.edu.upc.MLTDAH.iam.domain.model.queries.GetInstitutionByIdQuery;
+import pe.edu.upc.MLTDAH.iam.domain.model.queries.GetInstitutionByUserIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InstitutionQueryService {
+    List<Institution> handle(GetAllInstitutionsQuery query);
+    Optional<Institution> handle(GetInstitutionByIdQuery query);
+    Optional<Institution> handle(GetInstitutionByUserIdQuery query);
+}
