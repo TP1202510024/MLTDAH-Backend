8878fa2e069e8f4ab29a7e9eb145570db400a159
Djalma Dioses Molina <dioses1302@gmail.com>
2025-06-12 12:43:55 -0500

    feat: add student aggregate

---
+++ src/main/java/pe/edu/upc/MLTDAH/students/domain/model/aggregates/Student.java
@@ -0,0 +1,70 @@
+package pe.edu.upc.MLTDAH.students.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import pe.edu.upc.MLTDAH.iam.domain.model.aggregates.Institution;
+import pe.edu.upc.MLTDAH.students.domain.model.entities.Gender;
+import pe.edu.upc.MLTDAH.students.domain.model.entities.SchoolGrade;
+
+import java.util.Date;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Student extends AbstractAggregateRoot<Institution> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String firstName;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String lastName;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private Date birthDate;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String photo;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "schoolgrade_id")
+    @Getter
+    @Setter
+    private SchoolGrade schoolGrade;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "gender_id")
+    @Getter
+    @Setter
+    private Gender gender;
+
+    @ManyToOne
+    @JoinColumn(name = "institution_id")
+    @Getter
+    @Setter
+    private Institution institution;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    @Getter
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
+}
