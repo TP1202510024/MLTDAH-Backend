c7901783af47e5e7529d4a8e0686a8349c5ad470
Djalma Dioses Molina <dioses1302@gmail.com>
2025-06-12 14:37:56 -0500

    feat: add parent repository

---
+++ src/main/java/pe/edu/upc/MLTDAH/students/infrastructure/persistence/jpa/ParentRepository.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.MLTDAH.students.infrastructure.persistence.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.MLTDAH.students.domain.model.aggregates.Parent;
+
+import java.util.Optional;
+
+@Repository
+public interface ParentRepository extends JpaRepository<Parent, Long> {
+    Optional<Parent> findByStudentId(Long studentId);
+}
