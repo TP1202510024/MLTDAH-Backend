370aca66ab19ecf19d21dfb6f8ec6edf83a6b01f
Djalma Dioses Molina <dioses1302@gmail.com>
2025-06-12 14:05:35 -0500

    feat: add student controller

---
+++ src/main/java/pe/edu/upc/MLTDAH/students/interfaces/rest/StudentController.java
@@ -0,0 +1,146 @@
+package pe.edu.upc.MLTDAH.students.interfaces.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.MLTDAH.students.domain.model.aggregates.Student;
+import pe.edu.upc.MLTDAH.students.domain.model.commands.DeleteStudentCommand;
+import pe.edu.upc.MLTDAH.students.domain.model.queries.*;
+import pe.edu.upc.MLTDAH.students.domain.services.StudentCommandService;
+import pe.edu.upc.MLTDAH.students.domain.services.StudentQueryService;
+import pe.edu.upc.MLTDAH.students.interfaces.rest.resources.CreateStudentResource;
+import pe.edu.upc.MLTDAH.students.interfaces.rest.resources.UpdateStudentResource;
+import pe.edu.upc.MLTDAH.students.interfaces.rest.transform.CreateStudentCommandFromResourceAssembler;
+import pe.edu.upc.MLTDAH.students.interfaces.rest.transform.StudentResourceFromEntityAssembler;
+import pe.edu.upc.MLTDAH.students.interfaces.rest.transform.UpdateStudentCommandFromResourceAssembler;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+    private final StudentCommandService studentCommandService;
+    private final StudentQueryService studentQueryService;
+
+    public StudentController(StudentCommandService studentCommandService, StudentQueryService studentQueryService) {
+        this.studentCommandService = studentCommandService;
+        this.studentQueryService = studentQueryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addStudent(@RequestBody CreateStudentResource createStudentResource) {
+        try {
+            Optional<Student> student = studentCommandService.handle(CreateStudentCommandFromResourceAssembler.toCommandFromResource(createStudentResource));
+
+            if (student.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("message", "Failed to register student"));
+            }
+
+            return student.map(source -> ResponseEntity.ok(StudentResourceFromEntityAssembler.toResourceFromEntity(source))).orElseGet(() -> ResponseEntity.badRequest().build());
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("message", ex.getMessage()));
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateStudent(@PathVariable Long id, @RequestBody UpdateStudentResource updateStudentResource) {
+        try {
+            Optional<Student> student = studentCommandService.handle(UpdateStudentCommandFromResourceAssembler.toCommandFromResource(updateStudentResource), id);
+
+            if (student.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("message", "Failed to update student"));
+            }
+
+            return student.map(source -> ResponseEntity.ok(StudentResourceFromEntityAssembler.toResourceFromEntity(source))).orElseGet(() -> ResponseEntity.badRequest().build());
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("message", ex.getMessage()));
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllStudents() {
+        List<Student> students = studentQueryService.handle(new GetAllStudentsQuery());
+
+        if(students.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return ResponseEntity.ok(students.stream().map(StudentResourceFromEntityAssembler::toResourceFromEntity).toList());
+    }
+
+    @GetMapping("/institution/{institutionId}")
+    public ResponseEntity<?> getAllStudentsByInstitutionId(@PathVariable Long institutionId) {
+        List<Student> students = studentQueryService.handle(new GetAllStudentsByInstitutionIdQuery(institutionId));
+
+        if(students.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return ResponseEntity.ok(students.stream().map(StudentResourceFromEntityAssembler::toResourceFromEntity).toList());
+    }
+
+    @GetMapping("/institution/{institutionId}/grade/{gradeId}")
+    public ResponseEntity<?> getAllStudentsByInstitutionIdAndGradeId(@PathVariable Long institutionId, @PathVariable Long gradeId) {
+        List<Student> students = studentQueryService.handle(new GetAllStudentsByInstitutionIdAndGradeIdQuery(institutionId, gradeId));
+
+        if(students.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return ResponseEntity.ok(students.stream().map(StudentResourceFromEntityAssembler::toResourceFromEntity).toList());
+    }
+
+    @GetMapping("/institution/{institutionId}/gender/{genderId}")
+    public ResponseEntity<?> getAllStudentsByInstitutionIdAndGenderId(@PathVariable Long institutionId, @PathVariable Long genderId) {
+        List<Student> students = studentQueryService.handle(new GetAllStudentsByInstitutionIdAndGenderIdQuery(institutionId, genderId));
+
+        if(students.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return ResponseEntity.ok(students.stream().map(StudentResourceFromEntityAssembler::toResourceFromEntity).toList());
+    }
+
+    @GetMapping("/institution/{institutionId}/gender/{genderId}/grade/{gradeId}")
+    public ResponseEntity<?> getAllStudentsByInstitutionIdGenderIdAndGrade(@PathVariable Long institutionId, @PathVariable Long genderId, @PathVariable Long gradeId) {
+        List<Student> students = studentQueryService.handle(new GetAllStudentsByInstitutionIdAndGenderIdAndGradeIdQuery(institutionId, genderId, gradeId));
+
+        if(students.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return ResponseEntity.ok(students.stream().map(StudentResourceFromEntityAssembler::toResourceFromEntity).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable Long id) {
+        try {
+            Optional<Student> student = studentQueryService.handle(new GetStudentByIdQuery(id));
+
+            if(student.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }
+
+            return student.map(source -> ResponseEntity.ok(StudentResourceFromEntityAssembler.toResourceFromEntity(source))).orElseGet(() -> ResponseEntity.badRequest().build());
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("message", ex.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        try {
+            Optional<Student> student =  studentCommandService.handle(new DeleteStudentCommand(id));
+
+            if(student.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }
+
+            return student.map(source -> ResponseEntity.ok(StudentResourceFromEntityAssembler.toResourceFromEntity(source))).orElseGet(() -> ResponseEntity.badRequest().build());
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("message", ex.getMessage()));
+        }
+    }
+}
---
+++ src/main/java/pe/edu/upc/MLTDAH/students/interfaces/rest/transform/StudentResourceFromEntityAssembler.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.MLTDAH.students.interfaces.rest.transform;
+
+import pe.edu.upc.MLTDAH.iam.interfaces.rest.transform.InstitutionResourceFromEntityAssembler;
+import pe.edu.upc.MLTDAH.students.domain.model.aggregates.Student;
+import pe.edu.upc.MLTDAH.students.interfaces.rest.resources.StudentResource;
+
+public class StudentResourceFromEntityAssembler {
+    public static StudentResource toResourceFromEntity(Student student) {
+        return new StudentResource(student.getFirstName(), student.getLastName(), student.getBirthDate(), student.getPhoto(), SchoolGradeResourceFromEntityAssembler.toResourceFromEntity(student.getSchoolGrade()), GenderResourceFromEntityAssembler.toResourceFromEntity(student.getGender()), InstitutionResourceFromEntityAssembler.toResourceFromEntity(student.getInstitution()));
+    }
+}
